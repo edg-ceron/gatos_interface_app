be54fe86f25c835a422d9e8f0daa80f648a95fd1
Edgar Ceron <edg-ceron@hotmail.com>
2021-01-21 18:51:31 -0600

    Se integra servicio de api theCatApi para obtener un gato aleatoriamente, Se agrega validacion para redimendionar la imagen en caso de necesitarlo

--- src/main/java/GatosService.java
+++ src/main/java/GatosService.java
@@ -1,15 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import com.google.gson.Gson;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
-/**
- *
- * @author eceron
- */
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+
 public class GatosService {
-    public static void verGatos () {
-        
+    public static void verGatos() throws IOException {
+        //Vamos a traer datos de la api
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://api.thecatapi.com/v1/images/search")
+                .method("GET", null)
+                .build();
+        Response response = client.newCall(request).execute();
+
+        String elJson = response.body().string();
+
+        //cortar los corchetes
+        elJson = elJson.substring(1, elJson.length());
+        elJson = elJson.substring(0, elJson.length()-1);
+
+        // crear un objeto de la clase gato
+        Gson gson = new Gson ();
+        Gatos gatos = gson.fromJson(elJson, Gatos.class);
+
+        //redimencionar en caso de necesitar
+        Image image = null;
+        try {
+            URL url = new URL(gatos.getUrl());
+            image = ImageIO.read(url);
+
+            ImageIcon fondoGato = new ImageIcon(image);
+            if (fondoGato.getIconWidth() > 800) {
+                //Redimensionamos
+                Image fondo = fondoGato.getImage();
+                Image modificada = fondo.getScaledInstance(800, 600, Image.SCALE_SMOOTH);
+                fondoGato = new ImageIcon(modificada);
+
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
     }
 }
